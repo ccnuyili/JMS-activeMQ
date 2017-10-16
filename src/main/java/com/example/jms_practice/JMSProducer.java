805cd23f3d657a7b677b33292254e6f72dddce4a
yili <yili@le.com>
2017-10-16 16:12:25 +0800

    init

---
+++ src/main/java/com/example/jms_practice/JMSProducer.java
@@ -0,0 +1,88 @@
+package com.example.jms_practice;
+
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.jms.*;
+
+/*
+* 参考 http://blog.csdn.net/jiuqiyuliang/article/details/48608237
+* */
+@RestController
+public class JMSProducer {
+
+    //默认连接用户名
+    private static final String USERNAME = ActiveMQConnection.DEFAULT_USER;
+    //默认连接密码
+    private static final String PASSWORD = ActiveMQConnection.DEFAULT_PASSWORD;
+    //默认连接地址
+    private static final String BROKEURL = ActiveMQConnection.DEFAULT_BROKER_URL;
+    //发送的消息数量
+    private static final int SENDNUM = 10;
+
+    @RequestMapping(path = "/send",method = RequestMethod.GET)
+    public static void sendMessage() {
+        //连接工厂
+        ConnectionFactory connectionFactory;
+        //连接
+        Connection connection = null;
+        //会话 接受或者发送消息的线程
+        Session session;
+        //消息的目的地
+        Destination destination;
+        //消息生产者
+        MessageProducer messageProducer;
+        //实例化连接工厂
+        connectionFactory = new ActiveMQConnectionFactory(JMSProducer.USERNAME, JMSProducer.PASSWORD, JMSProducer.BROKEURL);
+
+        try {
+            //通过连接工厂获取连接
+            connection = connectionFactory.createConnection();
+            //启动连接
+            connection.start();
+            //创建session
+            session = connection.createSession(true, Session.AUTO_ACKNOWLEDGE);
+            //创建一个名称为HelloWorld的消息队列
+            destination = session.createQueue("HelloWorld");
+            //创建消息生产者
+            messageProducer = session.createProducer(destination);
+            //发送消息
+            sendTextMessage(session, messageProducer);
+
+            session.commit();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (JMSException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+    /**
+     * 发送消息
+     *
+     * @param session
+     * @param messageProducer 消息生产者
+     * @throws Exception
+     */
+    public static void sendTextMessage(Session session, MessageProducer messageProducer) throws Exception {
+        for (int i = 0; i < JMSProducer.SENDNUM; i++) {
+            //创建一条文本消息
+            TextMessage message = session.createTextMessage("ActiveMQ 发送消息" + i);
+            System.out.println("发送消息：Activemq 发送消息" + i);
+            //通过消息生产者发出消息
+            messageProducer.send(message);
+        }
+
+    }
+}
